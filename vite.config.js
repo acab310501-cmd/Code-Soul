8cf32e5f4e03d00a5c000406f0e8923b18a53ff7
Rick <acab310501@gmail.com>
2026-08-02 13:54:24 +0300

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Code-Soul/',  // Обязательно со слэшем в конце!
-  plugins: [react()],
+  base: '/Code-Soul/'
 })
