7b333fb33f36a7b828fef00b803412481f29af82
Rick <acab310501@gmail.com>
2026-08-02 13:33:35 +0300

    Add CI/CD for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,3 +10,11 @@ export default defineConfig({
   */
   base: "./",
 });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Code-Soul/',  // Обязательно со слэшем в конце!
+  plugins: [react()],
+})
